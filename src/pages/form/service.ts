cabbf9c0cba1dee8cb2f2cea6583c08615ea45cd
Jeremiahstockdale <stockdale.jerry@gmail.com>
2024-10-01 16:19:21 -0400

    refactor(spec): service file types

--- src/pages/form/service.ts
+++ src/pages/form/service.ts
@@ -33,7 +33,7 @@ const machine = createMachine({
       "done",
       "ready",
       reduce((ctx: FormMachineContext, ev: FormMachineEvent) => {
-        return { ...ctx, spec: ev.data }
+        return { ...ctx, spec: [ev.data] }
       }),
     ),
   ),
